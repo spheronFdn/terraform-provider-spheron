94cf3367c14b795e3e9459b8210cf4f2bb36d478
ilij4 <ilijammilosavljevic@gmail.com>
2023-06-30 17:03:50 +0200

    example updated

--- examples/full/full.tf
+++ examples/full/full.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "spheron" {
-  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiJjZmU0YmM0NTRmMzA0MTg5NDk0NzUyZmUyOGY4YTg3MDEzOWRhMGM3NzkxMjM4YzY2NDY3MGM0Y2NjZDBmNmNmN2ZkNDAwOGNkMDNjN2U1NGExZTA1OWJmYzIyZWYzZDZlN2YyNGNhYmM1NWI1ZDQ5OWY4OGNjOTk4MDc3MjY4OCIsImlhdCI6MTY4NjY2NzIwNiwiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.lpC-lSr7kxh1ITq7rUHfT4cd7eBRRMF1wKEVp5WAfaA"
+  token = ""
 }
 
-# data "spheron_organization" "org" {
-# }
+data "spheron_organization" "org" {
+}
 
 resource "spheron_instance" "instance_test" {
   image        = "crccheck/hello-world"
@@ -54,27 +54,27 @@ resource "spheron_instance" "instance_test" {
   replicas = 1
 }
 
-# resource "spheron_marketplace_instance" "instance_IPFS_test" {
-#   name = "IPFS"
-#   # machine_image = "Ventus Nano"
-#   region = "any"
+resource "spheron_marketplace_instance" "instance_IPFS_test" {
+  name = "IPFS"
+  # machine_image = "Ventus Nano"
+  region = "any"
 
-#   # persistent_storage = {
-#   #   class       = "HDD"
-#   #   mount_point = "/etc/data"
-#   #   size        = 2
-#   # }
+  persistent_storage = {
+    class       = "HDD"
+    mount_point = "/etc/data"
+    size        = 2
+  }
 
-#   storage  = 10
-#   cpu      = 1
-#   memory   = 2
-#   replicas = 1
-# }
+  storage  = 10
+  cpu      = 1
+  memory   = 2
+  replicas = 1
+}
 
-# resource "spheron_domain" "domain_test_ipfs" {
-#   name = "test.com"
-#   type = "domain"
+resource "spheron_domain" "domain_test_ipfs" {
+  name = "test.com"
+  type = "domain"
 
-#   instance_port = spheron_marketplace_instance.instance_IPFS_test.ports[0].container_port
-#   instance_id   = spheron_marketplace_instance.instance_IPFS_test.id
-# }
+  instance_port = spheron_marketplace_instance.instance_IPFS_test.ports[0].container_port
+  instance_id   = spheron_marketplace_instance.instance_IPFS_test.id
+}
